bce7ecd778398d69529a42c776fb6c4c4b89efa3
Daryl Grant <darylist@hotmail.com>
2019-11-06 16:06:43 -0700

    Initial Commit?

---
+++ InkblotTwitterbot2/InkBot.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InkBot
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(300, 300)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 3600000
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(319, 13)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerate.TabIndex = 1
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(318, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 60000
+        '
+        'InkBot
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(442, 319)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "InkBot"
+        Me.Text = "Inkblot Bot"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ InkblotTwitterbot2/InkBot.vb
@@ -0,0 +1,164 @@
+﻿
+Imports System.Net
+Imports System.IO
+Imports System.Text
+Public Class InkBot
+    Private mainimage As Bitmap
+    Private FilesInFolder As Integer
+    Private Minutes As Integer = 60
+    Private auth As LinqToTwitter.SingleUserAuthorizer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        auth = New LinqToTwitter.SingleUserAuthorizer
+        auth.CredentialStore = New LinqToTwitter.SingleUserInMemoryCredentialStore
+        'will need to add your twitter api keys to app.config, or hardcode them here to post to twitter.
+        'will also need to add a folder path to My.Settings.ImageDirectory in app.config
+        'this will be the directory where the inkblot images are stored.
+        'it will store every one at the moment, so this directory will grow with time.
+        'each 600x600 file is only typically between 40 and 63 kb in size.
+        auth.CredentialStore.ConsumerKey = My.Settings.ConsumerKey
+        auth.CredentialStore.ConsumerSecret = My.Settings.ConsumerSecret
+        auth.CredentialStore.OAuthToken = My.Settings.OAuthToken
+        auth.CredentialStore.OAuthTokenSecret = My.Settings.OAuthTokenSecret
+        btnGenerate_Click(sender, e)
+        Label1.Text = "" & Minutes & "Minutes until next tweet"
+    End Sub
+
+    'Private Function SendRequest(uri As Uri, jsonDataBytes As Byte(), contentType As String, method As String) As String
+    '    Dim response As String
+    '    Dim request As WebRequest
+
+    '    request = WebRequest.Create(uri)
+    '    request.ContentLength = jsonDataBytes.Length
+    '    request.ContentType = contentType
+    '    request.Method = method
+
+    '    Using requestStream = request.GetRequestStream
+    '        requestStream.Write(jsonDataBytes, 0, jsonDataBytes.Length)
+    '        requestStream.Close()
+
+    '        Using responseStream = request.GetResponse.GetResponseStream
+    '            Using reader As New StreamReader(responseStream)
+    '                response = reader.ReadToEnd()
+    '            End Using
+    '        End Using
+    '    End Using
+
+    '    Return response
+    'End Function
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim factor = 1
+        Dim BlackPen = New Pen(System.Drawing.Color.White, factor)
+        mainimage = New Bitmap(600, 600)
+        Dim g = Graphics.FromImage(mainimage)
+        Dim x, y As Integer
+        Dim ran1 As Integer
+        Dim rand As Random
+        rand = New Random()
+        x = 0
+        y = 300
+        g.Clear(Color.DeepSkyBlue)
+        While (x < 300 - factor And x > -300 + factor And y < 600 - factor And y > factor)
+            ran1 = rand.Next(4)
+            Select Case ran1
+                Case 0
+                    x = x + factor
+                Case 1
+                    x = x - factor
+                Case 2
+                    y = y + factor
+                Case 3
+                    y = y - factor
+            End Select
+            g.DrawRectangle(BlackPen, 300 + x, y, factor, factor)
+            g.DrawRectangle(BlackPen, 300 - x, y, factor, factor)
+        End While
+        PictureBox1.Image = mainimage
+        FilesInFolder = Directory.GetFiles(My.Settings.ImageDirectory, "*.Jpeg").Count
+        Try
+            mainimage.Save(My.Settings.ImageDirectory & FilesInFolder & ".jpeg", System.Drawing.Imaging.ImageFormat.Jpeg)
+
+        Catch ex As Exception
+            Console.WriteLine(ex.ToString)
+        End Try
+        Console.WriteLine("Picture Saved " & FilesInFolder & ".jpeg")
+
+        Try
+
+            ' Task.Run(SendTweet())
+            Dim action As Action = AddressOf SendTweet
+            Dim task = New Task(action)
+            task.Start()
+            task.Wait()
+            Console.WriteLine(task.Status.ToString)
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Sub
+
+    Private Async Function SendTweet() As Task(Of LinqToTwitter.Status)
+
+        ' Send Tweet!
+        'Moved the commented block below to form load to avoid a race condition where it tries to send the tweet
+        'before it even loads the credentials into the credentialstore
+        'Dim auth = New LinqToTwitter.SingleUserAuthorizer
+        'auth.CredentialStore = New LinqToTwitter.SingleUserInMemoryCredentialStore
+        'auth.CredentialStore.ConsumerKey = My.Settings.ConsumerKey
+        'auth.CredentialStore.ConsumerSecret = My.Settings.ConsumerSecret
+        'auth.CredentialStore.OAuthToken = My.Settings.OAuthToken
+        'auth.CredentialStore.OAuthTokenSecret = My.Settings.OAuthTokenSecret
+
+        'for a good c# equivalent of this code and the basics which I built the posting part, see this, part 3 as well for the images
+        'https://www.dougv.com/2015/08/posting-status-updates-to-twitter-via-linqtotwitter-part-2-plain-text-tweets/
+        'TODO: remove commented out testing code/refactored code
+
+        Dim context = New LinqToTwitter.TwitterContext(auth)
+        Dim uploadedMedia As LinqToTwitter.Media
+        'Try
+        '    uploadedMedia = Await context.UploadMediaAsync(File.ReadAllBytes("C:\\Users\\Daryl\\Pictures\\inkblotpics\\" & FilesInFolder & ".jpeg"), "image/jpeg", Nothing, Nothing, False, Nothing)
+        'Catch ex As Exception
+        '    MsgBox("Error uploading picture" & vbCrLf & ex.Message)
+        'Finally
+        '    MessageBox.Show(uploadedMedia.MediaID)
+        '    uploadedMedia.
+        'End Try
+        uploadedMedia = Await context.UploadMediaAsync(File.ReadAllBytes(My.Settings.ImageDirectory & FilesInFolder & ".jpeg"), "image/jpeg", Nothing, Nothing, False, Nothing)
+        'Console.WriteLine("File Uploaded " & FilesInFolder & ".jpeg")
+        Dim mediaIds As New List(Of ULong)
+        mediaIds.Clear()
+
+        mediaIds.Add(uploadedMedia.MediaID)
+        'MessageBox.Show(uploadedMedia.Status)
+        Dim s As LinqToTwitter.Status
+        s = Nothing
+        Try
+
+            s = Await context.TweetAsync("What do you see?", mediaIds)
+        Catch ex As Exception
+            Console.WriteLine("Error Sending tweet" & vbCrLf & ex.Message)
+
+
+        End Try
+        Return s
+
+    End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        'right now it is set up to send a tweet on launch of the program, and then every hour. change the if statement below
+        'if you want to change the interval.
+        If Minutes = 0 Then
+            btnGenerate_Click(sender, e)
+            'set minutes here to what you want the interval to be.
+            Minutes = 60
+        Else
+            Minutes = Minutes - 1
+        End If
+        Label1.Text = "" & Minutes & "Minutes until next tweet"
+    End Sub
+End Class
+
+
